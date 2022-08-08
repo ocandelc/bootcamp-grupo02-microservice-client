c35051ab988eacb80e48dcd14ff7dc560a07f8a4
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-08 11:04:47 -0500

    commit document client person

---
+++ client/src/main/java/com/nttdata/bootcamp/client/model/document/ClientPerson.java
@@ -0,0 +1,19 @@
+package com.nttdata.bootcamp.client.model.document;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@ToString
+@Document(collection="client_person")
+public class ClientPerson {
+
+    @Id
+    private String id;
+    private String codeClient;
+    private String codePerson;
+    private boolean state;
+
+}
