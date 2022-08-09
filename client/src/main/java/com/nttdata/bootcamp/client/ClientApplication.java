5d5be26cdedbda61e49b444f7167e943dc847dce
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-09 09:26:35 -0500

    commit comments client

--- client/src/main/java/com/nttdata/bootcamp/client/ClientApplication.java
+++ client/src/main/java/com/nttdata/bootcamp/client/ClientApplication.java
@@ -1,3 +1,17 @@
+/**
+ * Resumen.
+ * Objeto                   : ClientApplication.java
+ * Descripción              : Clase para iniciar el microservicio.
+ * Fecha de Creación        : 04/08/2022.
+ * Proyecto de Creación     : Bootcamp-01.
+ * Autor                    : Marvin Castro.
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Modificaciones
+ * Motivo                   Fecha             Nombre                  Descripción
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Bootcamp-01              05/08/2022        Oscar Candela           Realizar la creación de un método nuevo.
+ */
+
 package com.nttdata.bootcamp.client;
 
 import org.slf4j.Logger;
@@ -6,15 +20,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase para iniciar el microservicio.
+ */
 @SpringBootApplication
 public class ClientApplication implements CommandLineRunner {
 
+	/** Declaración de la variable de log */
 	private static final Logger log = LoggerFactory.getLogger(ClientApplication.class);
 
+	/** Creación del método main y sus argumentos */
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
+	/** Creación del método run y sus argumentos */
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Init Project");
